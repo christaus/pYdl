df7a2baa7b00587250e324cd8c08a344af4fdfc0
cgifl300 <cgifl300@gmail.com>
2020-08-13 12:51:49 +0200

    Code update

--- setup_gui.py
+++ setup_gui.py
@@ -51,11 +51,35 @@ class Setup_GUI(Toplevel):
         
         self.entete = image_set(self.panel_001, f"images{os.sep}logo-small")
         
+        parametres = [[_('Langue'),'language','lang','char'],
+                      [_('Couleur de fond texte'),'interface','couleur_fond','char'],
+                      [_('Couleur du texte'),'interface','couleur_fond_texte','char'],
+                      [_('Couleur de fond saisie'),'interface','couleur_fond_saisie','char'],
+                      [_('Couleur du texte saisie'),'interface','couleur_texte_saisie','char'],
+                      [_('Commande youtube-dl'),'youtube-dl','path','char'],
+                      [_('Départ téléchargements (heure)'),'active','from','digit'],
+                      [_('Fin des téléchargements (heure)'),'active','to','digit'],
+                      [_('Dossier destination des mp3'),'path','mp3','char'],
+                      [_('Dossier destination des vidéos'),'path','videos','char'],
+                      ]
+        self.composants = []
+        
+        for ligne in parametres:
+            temporaire = []
+            temporaire.append(Label(self.panel_002, text=ligne[0], bg=couleur_fond, fg=couleur_texte))
+            temporaire.append(Entry(self.panel_002, bg=couleur_fond_saisie, fg=couleur_texte_saisie))
+            self.composants.append(temporaire)
+        
         """ Implantation des composants
         """
         self.panel_001.pack(expand=True, fill=BOTH)
         self.panel_002.pack(expand=True, fill=BOTH)
         self.panel_003.pack(expand=True, fill=BOTH)
+        curseur = 0
+        for element in self.composants:
+            element[0].grid(row = curseur, column = 0)
+            element[1].grid(row = curseur, column = 1)
+            curseur +=1
         
     def apply_setup(self):
         """ Records setup on hard drive
